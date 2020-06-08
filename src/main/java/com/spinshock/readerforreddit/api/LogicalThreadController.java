c7c4cbaaf528abaee8508cb22ad92a4b95b484bf
Yavor Filipov <yavor.filipov@zuhlke.com>
2020-06-08 11:30:12 +0300

    Add LogicalThreadController Post Mapping and Request Mapping

--- src/main/java/com/spinshock/readerforreddit/api/LogicalThreadController.java
+++ src/main/java/com/spinshock/readerforreddit/api/LogicalThreadController.java
@@ -3,8 +3,12 @@ package com.spinshock.readerforreddit.api;
 import com.spinshock.readerforreddit.model.LogicalThread;
 import com.spinshock.readerforreddit.service.LogicalThreadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/logicalthread")
 @RestController
 public class LogicalThreadController {
 
@@ -15,7 +19,8 @@ public class LogicalThreadController {
         this.logicalThreadService = logicalThreadService;
     }
 
-    public void addLogicalThread(LogicalThread logicalThread) {
+    @PostMapping
+    public void addLogicalThread(@RequestBody LogicalThread logicalThread) {
         logicalThreadService.addLogicalThread(logicalThread);
     }
 }
--- src/main/java/com/spinshock/readerforreddit/model/LogicalThread.java
+++ src/main/java/com/spinshock/readerforreddit/model/LogicalThread.java
@@ -1,12 +1,15 @@
 package com.spinshock.readerforreddit.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 public class LogicalThread {
     private final UUID id;
     private final String content;
 
-    public LogicalThread(UUID id, String content) {
+    public LogicalThread(UUID id,
+                         @JsonProperty("content") String content) {
         this.id = id;
         this.content = content;
     }
